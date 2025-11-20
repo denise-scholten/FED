59e5d5e3c14128cd3293576907b5ac82e445284e
denise-scholten <denise.scholten@hva.nl>
2025-11-20 09:02:29 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,28 @@
 // JavaScript Document
 console.log("hi");
+// hamburger menu hulp van ella met de code// 
+
+const menu = document.getElementById("menu");
+const nav = document.querySelector("nav");
+
+const openBtn = document.getElementById("MenuOpen");
+const closeBtn = document.getElementById("MenuSluiten");
+
+// Menu openen
+
+openBtn.addEventListener("click", () => {
+   menu.hidden = false;
+   nav.classList.add("open")
+   })
+
+// Menu sluiten
+
+closeBtn.addEventListener("click", () => {
+   nav.classList.remove("open")
+     // Wacht tot de animatie klaar is voordat hidden terug wordt gezet
+        setTimeout(() => {
+            menu.hidden = true;
+        }, 300)
+    })
+
+ 
